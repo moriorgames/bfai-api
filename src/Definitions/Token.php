e93f7429c90982a289e34dbb610d3db84b96324d
Morior <moriorgames@gmail.com>
2018-06-01 08:47:22 +0200

    Add phpredis client and first version of battle repository

---
+++ src/Definitions/Token.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Definitions;
+
+class Token
+{
+    const LENGTH = 32;
+}
---
+++ src/Repository/BattleRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Repository;
+
+use App\Definitions\Token;
+use Predis\Client as RedisClient;
+
+class BattleRepository
+{
+    private $client;
+
+    public function __construct(RedisClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getAllBattles(): array
+    {
+        return $this->client->keys('*');
+    }
+
+    public function getBattleByToken(string $battleToken): ?array
+    {
+        return $this->isBattleToken($battleToken) ? json_decode($this->client->get($battleToken), true) : null;
+    }
+
+    public function battleExists(string $battleToken): bool
+    {
+        return $this->getBattleByToken($battleToken) !== null;
+    }
+
+    public function persistBattle(string $battleToken, array $data)
+    {
+        if ($this->isBattleToken($battleToken)) {
+            $this->client->set($battleToken, json_encode($data));
+        }
+    }
+
+    public function removeBattleByToken(string $battleToken)
+    {
+        if ($this->isBattleToken($battleToken)) {
+            $this->client->del($battleToken);
+        }
+    }
+
+    private function isBattleToken(string $battleToken): bool
+    {
+        return Token::LENGTH === strlen($battleToken);
+    }
+}
