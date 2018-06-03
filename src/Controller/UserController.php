eff253fa13e776088a29fcf59612382b9b2fbbdd
Morior <moriorgames@gmail.com>
2018-06-03 22:23:32 +0200

    Improve user resository to get create and remove data

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,8 +14,18 @@ class UserController
         $this->repository = $repository;
     }
 
-    public function get(?string $token)
+    public function get(string $token)
     {
-        return new JsonResponse($this->repository->findByToken($token));
+        $result = $this->repository->findByToken($token);
+        if ($result === null) {
+            $data = [
+                'battleToken' => 'ac90094b-3bfa-4d1c-9b4c-c2f50d5f7c28'
+            ];
+            $this->repository->persist($token, $data);
+        } else {
+            $this->repository->remove($token);
+        }
+
+        return new JsonResponse($result);
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 class UserRepository extends AbstractRedisRepository
 {
-    public function findByToken(?string $token): array
+    public function findByToken(string $token): ?array
     {
         if ($this->tokenValidator->validate($token)) {
 
@@ -17,5 +17,19 @@ class UserRepository extends AbstractRedisRepository
     public function getByToken(string $token): ?array
     {
         return json_decode($this->client->get($token), true);
+    }
+
+    public function persist(string $token, array $data)
+    {
+        if ($this->tokenValidator->validate($token)) {
+            $this->client->set($token, json_encode($data));
+        }
+    }
+
+    public function remove(string $token)
+    {
+        if ($this->tokenValidator->validate($token)) {
+            $this->client->del($token);
+        }
     }
 }
