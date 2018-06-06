9a288d5f3502e1e30d5bacf04af48377b0ac4f9f
Morior <moriorgames@gmail.com>
2018-06-06 06:59:14 +0200

    Refactor some routes to be compelling with the API documentation

--- src/App/Controller/DataController.php
+++ src/App/Controller/DataController.php
@@ -5,10 +5,22 @@ namespace App\Controller;
 use App\Fixtures\HeroesFixture;
 use App\Fixtures\SkillsFixture;
 use App\Fixtures\SkillsHeroesFixture;
+use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DataController
 {
+    /**
+     * Get battle by token
+     *
+     * @Route("/api/data", methods={"GET"})
+     *
+     * @SWG\Tag(name="data")
+     * @SWG\Response(response=200, description="Success!.")
+     *
+     * @return JsonResponse
+     */
     public function index()
     {
         return new JsonResponse([
