2a199a287408fd86c3d327b6364ad7682531c7b0
Morior <moriorgames@gmail.com>
2018-06-18 08:02:12 +0200

    Create array to battle factory

---
+++ src/App/Factories/ArrayToBattleStaticFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Factories;
+
+use App\DTO\Battle;
+use App\DTO\BattleHero;
+use App\DTO\SkillHero;
+
+class ArrayToBattleStaticFactory
+{
+    public static function create(array $data): Battle
+    {
+        $battle = new Battle($data['battleToken'], $data['status']);
+        foreach ($data['heroes'] as $hero) {
+            $battleHero = new BattleHero(
+                $hero['userToken'], $hero['side'], $hero['battleHeroId'], $hero['heroId'], $hero['x'], $hero['y']
+            );
+            $battle->addBattleHero(
+                $battleHero
+            );
+        }
+        foreach ($data['skillsHeroes'] as $skill) {
+            $skillHero = new SkillHero;
+            $battle->addSkillHero(
+                $skillHero
+            );
+        }
+
+        return $battle;
+    }
+}
