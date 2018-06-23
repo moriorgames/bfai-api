565d88781e688c6afd320c1f8fd6e474a37d3ea7
Morior <moriorgames@gmail.com>
2018-06-23 06:16:13 +0200

    Fix bug with nexus assign side and refactor battle merger

--- src/App/Services/BattleMergerBySide.php
+++ src/App/Services/BattleMergerBySide.php
@@ -8,31 +8,48 @@ use App\DTO\Battle;
 
 class BattleMergerBySide
 {
+    private $battleHeroId = 1;
+
+    private $heroTransformer;
+
+    public function __construct()
+    {
+        $this->heroTransformer = new BattleHeroArrayTransformer;
+    }
+
     public function merge(Battle $local, Battle $visitor): Battle
     {
         $battle = new Battle($local->getBattleToken(), BattleStatus::IN_PROGRESS);
-        $battleHeroId = 1;
-        $battleHeroId = $this->assignBattleHeroes($battleHeroId, $battle, $local, GameConstants::SIDE_LOCAL);
-        $battleHeroId = $this->assignBattleHeroes($battleHeroId, $battle, $visitor, GameConstants::SIDE_VISITOR);
+        $this->assignNexus($battle, $local);
+        $this->assignBattleHeroes($battle, $local, GameConstants::SIDE_LOCAL);
+        $this->assignBattleHeroes($battle, $visitor, GameConstants::SIDE_VISITOR);
 
         return $battle;
     }
 
-    private function assignBattleHeroes(int $battleHeroId, Battle $master, Battle $slave, string $side): int
+    private function assignNexus(Battle $master, Battle $slave): void
     {
-        $battleHeroArrayTransformer = new BattleHeroArrayTransformer;
         foreach ($slave->getBattleHeroes() as $hero) {
-            // Assign Nexus only one time for local and visitor
-            if ($hero['heroId'] == 1 && $battleHeroId > 2) {
-                continue;
+            if ($hero['heroId'] == 1) {
+                $this->addHeroToBattle($master, $hero, $hero['side']);
             }
-
-            $battleHero = $battleHeroArrayTransformer->transform($battleHeroId, $hero);
-            $battleHero->setSide($side);
-            $master->addBattleHero($battleHero);
-            $battleHeroId++;
         }
+    }
 
-        return $battleHeroId;
+    private function assignBattleHeroes(Battle $master, Battle $slave, string $side): void
+    {
+        foreach ($slave->getBattleHeroes() as $hero) {
+            if ($hero['heroId'] > 1) {
+                $this->addHeroToBattle($master, $hero, $side);
+            }
+        }
+    }
+
+    private function addHeroToBattle(Battle $battle, array $hero, string $side): void
+    {
+        $battleHero = $this->heroTransformer->transform($this->battleHeroId, $hero);
+        $battleHero->setSide($side);
+        $battle->addBattleHero($battleHero);
+        $this->battleHeroId++;
     }
 }
